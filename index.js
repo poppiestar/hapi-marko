6f29996239f78b2a19fec1844ce056ef7379eba7
Drew Miller <drew@wwdrew.com>
2015-03-20 15:22:23 +0000

    Rendering a basic template

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 var marko;
-var fs = require('fs');
-var Path = require('path');
 
 try {
 	marko = require('marko');
@@ -28,22 +26,11 @@ var getTemplateExt = function(options) {
 module.exports = {
 	module: {
 		compile: function(template, options, callback) {
-			dust.onLoad = function(name, callback) {
-				var templateFile = Path.join(getBaseDir(options), name + getTemplateExt(options));
-				fs.readFile(templateFile, function(err, data) {
-					if (err) {
-						throw err;
-					}
-
-					callback(err, data.toString());
-				});
-			}
-
-			var compiled = marko.compileFn(template, options && options.name);
+			var template = marko.load(options.filename);
 
 			process.nextTick(function () {
 				callback(null, function(context, options, callback) {
-					compiled(context, callback);
+					template.render(context, callback);
 				});
 			});
 		}
