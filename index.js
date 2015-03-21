5aa7378e4ee08a38a4dd24d45ed9178b69e78f97
Drew Miller <drew@onpatrol.co>
2015-03-21 00:52:12 +0000

    Removed redundant functions and fixed lint errors

--- index.js
+++ index.js
@@ -7,34 +7,18 @@ catch (ex) {}
 
 if (!marko) throw new Error('"marko" module not found');
 
-var getBaseDir = function(options) {
-	if (options && options.baseDir) {
-		return options.baseDir;
-	}
-
-	return '';
-}
-
-var getTemplateExt = function(options) {
-	if (options && options.defaultExt && options.defaultExt.length > 0) {
-		return '.' + options.defaultExt;
-	}
-
-	return '.marko';
-}
-
 module.exports = {
 	module: {
 		compile: function(template, options, callback) {
-			var template = marko.load(options.filename);
+			var loadedTemplate = marko.load(options.filename);
 
 			process.nextTick(function () {
 				callback(null, function(context, options, callback) {
-					template.render(context, callback);
+					loadedTemplate.render(context, callback);
 				});
 			});
 		}
 	},
 	compileMode: 'async'
-}
+};
 
